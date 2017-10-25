4ca8ca1279400c30ff9b5c9b3c2c01ebf97d2ce0
Wyswist <bartlomiejpiotrowski1606@gmail.com>
2017-10-25 17:06:11 +0200

    charts works bigMeanAcc and meanAcc

--- app/src/main/java/com/example/bartlomiej/myapplication/MainActivity.java
+++ app/src/main/java/com/example/bartlomiej/myapplication/MainActivity.java
@@ -14,7 +14,10 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
+import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private TextView xText, yText, zText, roadText;
     private GraphView graphView;
+    private GridLabelRenderer gridLabelRenderer;
     private Sensor mySensor;
     private SensorManager SM;
     private RoadCounter roadCounter;
@@ -35,8 +39,51 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private float[] acc;
     long startTime = 0L, timeInMilliseconds = 0L, updateTime = 0L, timeSwapBuff = 0L;
     Handler customHandler = new Handler();
-    ExecutorService updateTimeThreadExecutor =  Executors.newSingleThreadExecutor();
+    ExecutorService updateTimeThreadExecutor = Executors.newSingleThreadExecutor();
     LineGraphSeries<DataPoint> graphSeries;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        roadCounter = new RoadCounter();
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_GAME);
+
+        xText = (TextView)findViewById(R.id.xText);
+        yText = (TextView)findViewById(R.id.yText);
+        zText = (TextView)findViewById(R.id.zText);
+        roadText = (TextView)findViewById(R.id.Road);
+
+        startButton = (Button)findViewById(R.id.START);
+        stopButton  = (Button)findViewById(R.id.STOP);
+        resizeButton  = (Button)findViewById(R.id.Resize);
+
+        graphView = (GraphView)findViewById(R.id.graph);
+        graphSeries  = new LineGraphSeries<DataPoint>();
+        graphView.getViewport().setYAxisBoundsManual(true);
+//        graphView.getViewport().setMinY(-5);
+//        graphView.getViewport().setMaxY(+5);
+        graphView.getViewport().setScalableY(true);
+        graphView.getViewport().setScrollableY(true);
+//        graphView.getViewport().setScrollable(true);
+        gridLabelRenderer = graphView.getGridLabelRenderer();
+        startButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startTime = SystemClock.uptimeMillis();
+                customHandler.postDelayed(updateTimeThread, 0);
+            }
+        });
+        stopButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                updateTimeThreadFuture.cancel(true);
+                customHandler.removeCallbacks(updateTimeThread);
+                startTime = 0;
+                roadCounter.reset();
+            }
+        });
+    }
 
     Runnable updateTimeThread = new Runnable(){
         public void run(){
@@ -52,59 +99,55 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 //                    + String.format("%3d", milliseconds) + "\n"
 //                    + "Przebyta droga: " + roadCounter.road+"m" + "\n"
 //                    + "aktualny czas: " + timeInMilliseconds + "\n"
-                    + "pochodna przyspieszenia: " + roadCounter.da_by_dt + "\n"
-                    + "średnia pochodna przyspieszenia: " + roadCounter.da_by_dtMean + "\n"
-                    +  "Licznik" + roadCounter.signChangeCounter);
-//            roadText.setText(String.format("%2d", secs) + ":" + String.format("%3d", milliseconds));
-            graphSeries.appendData(new DataPoint(milliseconds, acc[0]), true, 1500);
+//                    + "pochodna przyspieszenia: " + roadCounter.da_by_dt + "\n"
+//                    + "średnia pochodna przyspieszenia: " + roadCounter.da_by_dtMean + "\n"
+//                    + "getHighestValueX: " + graphSeries.getHighestValueX()+ "\n"
+                      + "Srednia z przyspieszenia  : " + roadCounter.accMean+ "\n"
+                      + "Srednia z przyspieszenia2: " + roadCounter.bigAccMean+ "\n"
+                      +  "Licznik" + roadCounter.signChangeCounter);
+            graphView.getViewport().setXAxisBoundsManual(true);
+            graphView.getViewport().setMinX(secs-10000);
+            graphView.getViewport().setMaxX(secs);
+            graphView.getViewport().setMinY(roadCounter.accMean - 1.5);
+            graphView.getViewport().setMaxY(roadCounter.accMean + 1.5);
+            graphSeries.appendData(new DataPoint(timeInMilliseconds, roadCounter.accMean), true, 900);
             graphView.addSeries(graphSeries);
-            if(graphSeries.getHighestValueX()>1499){
-                graphView.removeAllSeries();
+            graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+                @Override
+                public String formatLabel(double value, boolean isValueX) {
+                    if(isValueX)
+                        return super.formatLabel((int)(value/1000), isValueX)+ "s";
+                    else
+                        return super.formatLabel(value, isValueX);
+                }
+            });
+
+            if(graphSeries.getHighestValueX() % 10000 == 0){
+//                customHandler.postDelayed(onReset, 0);
             }
             customHandler.postDelayed(this, 0);
-           }
+        }
     };
 
-    Future updateTimeThreadFuture = updateTimeThreadExecutor.submit(updateTimeThread);
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        roadCounter = new RoadCounter();
-        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
-        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    Runnable onReset = new Runnable(){
+        public void run(){
+            graphSeries.resetData(generateData());
+            customHandler.postDelayed(this, 10000);
+        }
+    };
 
-        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_GAME);
-
-        xText = (TextView)findViewById(R.id.xText);
-        yText = (TextView)findViewById(R.id.yText);
-        zText = (TextView)findViewById(R.id.zText);
-        roadText = (TextView)findViewById(R.id.Road);
-
-        startButton = (Button)findViewById(R.id.START);
-        stopButton  = (Button)findViewById(R.id.STOP);
-        resizeButton  = (Button)findViewById(R.id.Resize);
-
-        graphView = (GraphView)findViewById(R.id.graph);
-        graphSeries  = new LineGraphSeries<DataPoint>();
-
-        startButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                startTime = SystemClock.uptimeMillis();
-                customHandler.postDelayed(updateTimeThread,0);
-            }
-        });
-
-        stopButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-
-                updateTimeThreadFuture.cancel(true);
-                customHandler.removeCallbacks(updateTimeThread);
-                startTime=0;
-                roadCounter.reset();
-            }
-        });
+    private DataPoint[] generateData() {
+        int count = 1;
+        DataPoint[] values = new DataPoint[count];
+        for (int i=0; i<count; i++) {
+            double x = i+10;
+            DataPoint v = new DataPoint(timeInMilliseconds-10000+i,7);
+            values[i] = v;
+        }
+        return values;
     }
+    Future updateTimeThreadFuture = updateTimeThreadExecutor.submit(updateTimeThread);
+
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
--- app/src/main/java/com/example/bartlomiej/myapplication/RoadCounter.java
+++ app/src/main/java/com/example/bartlomiej/myapplication/RoadCounter.java
@@ -12,13 +12,17 @@ public class RoadCounter {
     public double velocity;
     private double i_minus_one_time;
     Vector<Float> accVec;
+    public double accMean;
+    public double bigAccMean;
     Vector<Double> da_by_dtVector;
     Vector<Double> deltaTimeVector;
+    Vector<Float> constAccVector;
     public int signChangeCounter;
     public double da_by_dt;
     public double da_by_dtMean;
     float acceleration_i_minus_one;
-    int buffor = 6000;
+    int buffor = 2;
+    int constBuffor = 10;
 
     public RoadCounter(){
         road = 0;
@@ -29,8 +33,10 @@ public class RoadCounter {
         da_by_dtMean = 0;
         acceleration_i_minus_one = 0;
         accVec = new Vector<Float>(buffor);
+        accMean = 0;
         da_by_dtVector = new Vector<Double>(buffor);
         deltaTimeVector = new Vector<Double>(buffor);
+        constAccVector = new Vector<Float>(constBuffor);
         for (int i=0; i<accVec.capacity(); i++){
             accVec.add(0.0f);
         }
@@ -39,6 +45,9 @@ public class RoadCounter {
         }
         for (int i=0; i<deltaTimeVector.capacity(); i++){
             deltaTimeVector.add(0.0);
+        }
+        for (int i=0; i<constAccVector.capacity(); i++){
+            constAccVector.add(0.0f);
         }
     }
 
@@ -57,6 +66,9 @@ public class RoadCounter {
         for (int i=0; i<accVec.capacity(); i++){
             accVec.add(0.0f);
         }
+        for (int i=0; i<constAccVector.capacity(); i++){
+            constAccVector.add(0.0f);
+        }
         for (int i=0; i<da_by_dtVector.capacity(); i++){
             da_by_dtVector.add(0.0);
         }
@@ -66,13 +78,26 @@ public class RoadCounter {
     }
 
     public void CountRoad(float[] acceleration, double i_time){
-        if(acceleration[1]< 9.2 && acceleration[0]>2)
-            return;
+//        if(acceleration[1]< 9.2 && acceleration[0]>2)
+//            return;
         double deltaTime = (i_time - i_minus_one_time)/1000;
-        accChange(acceleration, deltaTime);
-        velocity += Math.abs(acceleration[0]) * deltaTime;
-        road  += velocity * deltaTime;
-        i_minus_one_time = i_time;
+//        accChange(acceleration, deltaTime);
+        for(int i = accVec.capacity()-1; i >= 1; i--){
+            accVec.set(i,accVec.elementAt(i-1));
+        }
+        accVec.set(0,acceleration[0]);
+        double sumOfAcc=0;
+        for(double acc : accVec)
+        {
+            sumOfAcc+=acc;
+        }
+        accMean = sumOfAcc/buffor;
+//        if(accMean>1)
+//            accMean = 0;
+        constMeanReset(acceleration);
+//        velocity += Math.abs(acceleration[0]) * deltaTime;
+//        road  += velocity * deltaTime;
+//        i_minus_one_time = i_time;
     }
 
     private void accChange(float[] acceleration, double deletaTime){
@@ -87,62 +112,82 @@ public class RoadCounter {
             deltaTimeVector.set(i, deltaTimeVector.elementAt(i-1));
         }
         deltaTimeVector.set(0,deletaTime);
-            float accSum = 0;
-            double deltaSum = 0;
 
-            // wyciaganie sredniej
-            for (float acc : accVec){
-                accSum += acc;
-                if (acc>0)
-                    plusSign = true;
-                if(acc<0)
-                    minusSign = true;
-            }
-            for (double delta : deltaTimeVector){
-                deltaSum += delta;
-            }
-            da_by_dtMean = Math.round(accSum/deltaSum);
+        float accSum = 0;
+        double deltaSum = 0;
 
-            if(plusSign == true && minusSign == true && Math.abs(da_by_dtMean) > 35 && Math.abs(da_by_dtMean)< 160)
-            {
-                signChangeCounter++;
-                deltaTimeVector.clear();
-                da_by_dtVector.clear();
-                accVec.clear();
-                for (int i=0; i<accVec.capacity(); i++){
-                    accVec.add(0.0f);
-                }
-                for (int i=0; i<da_by_dtVector.capacity(); i++){
-                    da_by_dtVector.add(0.0);
-                }
-                for (int i=0; i<deltaTimeVector.capacity(); i++){
-                    deltaTimeVector.add(0.0);
-                }
-            }
+        // wyciaganie sredniej
+        for (float acc : accVec){
+            accSum += acc;
+            if (acc>0)
+                plusSign = true;
+            if(acc<0)
+                minusSign = true;
+        }
+        for (double delta : deltaTimeVector){
+            deltaSum += delta;
+        }
+        da_by_dtMean = Math.round(accSum/deltaSum);
 
-            if(!accVec.contains(0)){
-                accVec.clear();
-                for (int i=0; i<accVec.capacity(); i++){
-                    accVec.add(0.0f);
-                }
+        if(plusSign == true && minusSign == true && Math.abs(da_by_dtMean) > 2)
+        {
+            signChangeCounter++;
+            deltaTimeVector.clear();
+            da_by_dtVector.clear();
+            accVec.clear();
+            for (int i=0; i<accVec.capacity(); i++){
+                accVec.add(0.0f);
             }
-            if(!da_by_dtVector.contains(0)){
-                da_by_dtVector.clear();
-                for (int i=0; i<da_by_dtVector.capacity(); i++){
-                    da_by_dtVector.add(0.0);
-                }
+            for (int i=0; i<da_by_dtVector.capacity(); i++){
+                da_by_dtVector.add(0.0);
             }
-            if(!deltaTimeVector.contains(0)){
-                deltaTimeVector.clear();
-                for (int i=0; i<deltaTimeVector.capacity(); i++){
-                    deltaTimeVector.add(0.0);
-                }
+            for (int i=0; i<deltaTimeVector.capacity(); i++){
+                deltaTimeVector.add(0.0);
             }
+        }
+
+//        if(!accVec.contains(0)){
+//            accVec.clear();
+//            for (int i=0; i<accVec.capacity(); i++){
+//                accVec.add(0.0f);
+//            }
+//        }
+//        if(!da_by_dtVector.contains(0)){
+//            da_by_dtVector.clear();
+//            for (int i=0; i<da_by_dtVector.capacity(); i++){
+//                da_by_dtVector.add(0.0);
+//            }
+//        }
+//        if(!deltaTimeVector.contains(0)){
+//            deltaTimeVector.clear();
+//            for (int i=0; i<deltaTimeVector.capacity(); i++){
+//                deltaTimeVector.add(0.0);
+//            }
+//        }
     }
 
     public void resizeVectors(){
         accVec.setSize(buffor);
         da_by_dtVector.setSize(buffor);
         deltaTimeVector.setSize(buffor);
+    }
+
+    public void constMeanReset(float[] acceleration){
+        float sum = 0;
+        for(int i = constAccVector.capacity()-1; i >= 1; i--){
+            constAccVector.set(i,constAccVector.elementAt(i-1));
+        }
+        constAccVector.set(0, acceleration[0]);
+        for (float acc : constAccVector){
+            sum += acc;
+        }
+        bigAccMean = sum / constBuffor;
+        if(Math.abs(accMean) <= Math.abs(bigAccMean) + 0.02f){
+            accMean = 0;
+            accVec.clear();
+            for (int i=0; i<accVec.capacity(); i++){
+                accVec.add(0.0f);
+            }
+        }
     }
 }
